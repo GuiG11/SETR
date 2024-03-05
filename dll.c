dc15855c95dee730fd43a0b8df573b0b79f2fdfd
Simao <simao.pinto@ua.pt>
2024-03-05 10:15:37 +0000

    dll.h dll.c

--- dll.c
+++ dll.c
@@ -4,3 +4,26 @@
 #include "dll.h"
 
 
+void MyDLLInit() {
+
+}
+
+int MyDLLInsert(uint16_t nmec, char *firsName, char *lastName, uint16_t age, float grade) {
+
+}
+
+int MyDLLRemove(uint16_t nmec) {
+
+}
+
+void MyDLLFind(uint16_t nemc) {
+
+}
+
+void MyDLLFindNext() {
+
+}
+
+void MyDLLFindPrevious() {
+    
+}
