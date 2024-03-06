80ba4239e62174ee3b39daf8e45f1e6f3f1f69be
Simao <simao.pinto@ua.pt>
2024-03-06 18:09:50 +0000

    dll.c update

--- Trab1/src/dll.c
+++ Trab1/src/dll.c
@@ -19,7 +19,16 @@ int MyDLLInsert(DLL *dll, uint16_t key, uint8_t data[]) {
         return EXIT_FAILURE;
     }
 
-    Node* newNode = &dll->elements[dll->num_elements];
+    Node* current = dll->head;
+    while(current != NULL) {
+        if (current->key == key) {
+            printf("Id already exists!\n");
+            return EXIT_FAILURE;
+        }
+        current = current->next;
+    }
+
+    Node* newNode = &dll->elements[dll->num_elements]; // Atribui o endereço de memória do próximo nó disponível no array de elementos
     newNode->key = key;
     for (int i = 0; i < MAX_DATA_SIZE; i++) {
         newNode->data[i] = data[i];
@@ -28,13 +37,13 @@ int MyDLLInsert(DLL *dll, uint16_t key, uint8_t data[]) {
     newNode->next = NULL;
     newNode->prev = NULL;
     
-    if (dll->head == NULL) {
+    if (dll->head == NULL) {  // Se a lista estiver vazia o novo nó torna-se tanto o head como o tail da DLL
         dll->head = newNode;
-        dll->tail = newNode;
+        dll->tail = newNode;  
     } else {
-        dll->tail->next = newNode;
-        newNode->prev = dll->tail;
-        dll->tail = newNode;
+        dll->tail->next = newNode;  // O ponteiro next do último nó (tail) aponta para o novo nó
+        newNode->prev = dll->tail;  // O ponteiro prev do novo nó aponta para o último nó
+        dll->tail = newNode;        // O último nó aponta para o novo nó
     }
 
     dll->num_elements++;
@@ -57,7 +66,7 @@ void MyDLLShowElements(DLL *dll) {
     Node* current = dll->head;
     printf("Doubly Linked List elements: \n");
     while(current != NULL) {
-        printf("ID: %d, Name: %s\n", current->key, current->data);
+        printf("Id: %d, Name: %s\n", current->key, current->data);
         current = current->next;
     }
 }
