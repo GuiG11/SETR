135fd6a594679d011118461dbb397277a33cde9c
Simao <simao.pinto@ua.pt>
2024-03-08 14:48:45 +0000

    CMakeLists created

--- Trab1/html/search/groups_0.js
+++ Trab1/html/search/groups_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['doubly_20linked_20list_20_28dll_29_42',['Doubly Linked List (dll)',['../group__dll.html',1,'']]]
+  ['doubly_20linked_20list_20_28dll_29_0',['Doubly Linked List (dll)',['../group__dll.html',1,'']]]
 ];
--- Trab1/html/search/variables_3.js
+++ Trab1/html/search/variables_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['key_37',['key',['../group__dll.html#ga868764e64becfb820d70f261153984f5',1,'Node']]]
+  ['key_0',['key',['../group__dll.html#ga868764e64becfb820d70f261153984f5',1,'Node']]]
 ];
