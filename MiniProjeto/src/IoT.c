e2d8bc81878b75ae63d963122f3195403eb36494
Simao <simao.pinto@ua.pt>
2024-05-21 10:54:00 +0100

    add CMakeLists

---
+++ MiniProjeto/src/IoT.c
@@ -0,0 +1,18 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+
+
+
+
+
+
+
+
+
+int main (void)
+{
+    
+}
